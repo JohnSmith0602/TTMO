a44825ccc6a45ceb873d896368ba6665e6eae6fd
John Smith <johnsmith20100602@gmail.com>
2014-07-29 02:10:38 +0800

    Remove database.

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var express = require('express'),
     routes = require('./routes'),
     http = require('http'),
     path = require('path'),
-    MongoStore = require('connect-mongo')(express),
-    db = require('./models/dbMongoose'),
+//    MongoStore = require('connect-mongo')(express),
+//    db = require('./models/dbMongoose'),
     settings = require('./settings');
 
 
@@ -23,22 +23,22 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser());
-app.use(express.session({
-    secret: settings.cookieSecret,
-    store: new MongoStore({
-        db: settings.db
-    })
-}));
+//app.use(express.session({
+//    secret: settings.cookieSecret,
+//    store: new MongoStore({
+//        db: settings.db
+//    })
+//}));
 
 // 什么时候执行？
-app.use(function (req, res, next) {
-    console.log('Into use res.locals...');
-    res.locals.success = req.session.success || null;
-    res.locals.error = req.session.error || null;
-    req.session.success = null;
-    req.session.error = null;
-    next();
-});
+//app.use(function (req, res, next) {
+//    console.log('Into use res.locals...');
+//    res.locals.success = req.session.success || null;
+//    res.locals.error = req.session.error || null;
+//    req.session.success = null;
+//    req.session.error = null;
+//    next();
+//});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -50,7 +50,7 @@ if ('development' == app.get('env')) {
 routes(app);
 
 app.on('close', function (err) {
-    db.disconnect();
+//    db.disconnect();
 });
 
 http.createServer(app).listen(app.get('port'), function(){
